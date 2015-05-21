36288de9b7a63ef6f0b163917e6ecc734d189829
Fabian Bergmark <fabian.bergmark@gmail.com>
2015-05-21 11:52:27 +0200

    Small fix

--- app/src/main/java/io/flic/demo/app/ui/MainActivity.java
+++ app/src/main/java/io/flic/demo/app/ui/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity {
     }
 
     private void setupWhitelist() {
-        FlicApplication.getApp().whitelistDeviceId("ATsA");
+        FlicApplication.getApp().whitelistDeviceId("");
     }
 
     @Override
